fb279f3d419676bce6588c7fecbe91cb5ff72c19
Eshwar <echandra@kiwiup.com>
2021-02-01 00:07:07 +0530

    updates DB connection

--- conn.php
+++ conn.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
-$con = mysql_connect("localhost","root","");
+$con = mysql_connect("localhost","thejbbiu_main","Lucky@2016");
 if (!$con)
 {
 	die("Could not connect!");
 }
-mysql_select_db("tamarind",$con) or header('Location:install.php');
+mysql_select_db("thejbbiu_tamarind",$con) or header('Location:install.php');
 ?>
