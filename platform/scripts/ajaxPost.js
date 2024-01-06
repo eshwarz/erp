74e327807c50030c74b8ffe0ac08a1520a703e7c
Eshwar Chandra <mail@eshwar.me>
2024-01-06 23:54:34 +0530

    Exception handlin

--- platform/scripts/ajaxPost.js
+++ platform/scripts/ajaxPost.js
@@ -1,12 +1,16 @@
 function ajaxPost(page,data,resultContainer)
 {
-	var dataArray = new Array();
-	dataArray = data.split(",");
-	console.log("🚀 ~ file: ajaxPost.js:5 ~ dataArray:", dataArray);
-	var dataString = "?";
-	for (var m=0;m<dataArray.length;m++)
-	{
-		dataString = dataString+dataArray[m]+"="+document.getElementById(dataArray[m]).value+"&";
+	try {
+		var dataArray = new Array();
+		dataArray = data.split(",");
+		console.log("🚀 ~ file: ajaxPost.js:5 ~ dataArray:", dataArray);
+		var dataString = "?";
+		for (var m=0;m<dataArray.length;m++)
+		{
+			dataString = dataString+dataArray[m]+"="+document.getElementById(dataArray[m]).value+"&";
+		}
+		ajaxpage(page+dataString,resultContainer);
+	} catch (e) {
+		console.log(e);
 	}
-	ajaxpage(page+dataString,resultContainer);
 }
