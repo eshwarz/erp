d428536210be661ed607f8e6c8f6f3c532aec909
Eshwar Chandra <mail@eshwar.me>
2024-01-06 22:03:15 +0530

    Fix

--- custom_scripts/get_village.js
+++ custom_scripts/get_village.js
@@ -2,7 +2,8 @@ function getVillage(farmerId,targetId,showResult)
 {
 	var fid = $("#"+farmerId).val();
 	console.log("weight_list/get_village.php?farmer_id="+fid);
-	$("#"+targetId).load("weight_list/get_village.php?farmer_id="+fid,function(){
+	$("#"+targetId).load("weight_list/get_village.php?farmer_id="+fid,function(e){
+		console.log(e);
 		//$("#"+showResult).css({"visibility":"visible"});
 		$("#"+showResult).css({"display":"block"});
 		setTimeout(function (){
