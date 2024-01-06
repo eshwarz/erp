17161e14bd56b5cc5f89c0f6086e6e712b1ec053
Eshwar Chandra <eshwar@useblitz.com>
2024-01-06 18:31:22 +0530

    fix

--- custom_scripts/get_village.js
+++ custom_scripts/get_village.js
@@ -1,6 +1,7 @@
 function getVillage(farmerId,targetId,showResult)
 {
 	var farmer_id = $("#"+farmerId).val();
+	console.log("weight_list/get_village.php?farmer_id="+farmer_id);
 	$("#"+targetId).load("weight_list/get_village.php?farmer_id="+farmer_id,function(){
 		//$("#"+showResult).css({"visibility":"visible"});
 		$("#"+showResult).css({"display":"block"});
