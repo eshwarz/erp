d80fd7c81f2a39754e052a6f8dfc14970e003635
Eshwar Chandra <mail@eshwar.me>
2024-01-06 22:05:16 +0530

    fix

--- custom_scripts/get_village.js
+++ custom_scripts/get_village.js
@@ -1,16 +1,15 @@
 function getVillage(farmerId,targetId,showResult)
 {
 	var fid = $("#"+farmerId).val();
-	console.log("weight_list/get_village.php?farmer_id="+fid);
-	$("#"+targetId).load("weight_list/get_village.php?farmer_id="+fid,function(e){
-		console.log(e);
-		//$("#"+showResult).css({"visibility":"visible"});
-		$("#"+showResult).css({"display":"block"});
-		setTimeout(function (){
-			var dataFarmerId = $('#data-farmer-id').attr('data-farmer-id');
-			$('#farmer').attr('value', dataFarmerId);
-			$('#farmer option[value="' + dataFarmerId + '"]').prop('selected', true);
-		});
+	$("#"+targetId).load("weight_list/get_village.php?farmer_id="+fid,function(response){
+		if (response) {
+			$("#"+showResult).css({"display":"block"});
+			setTimeout(function (){
+				var dataFarmerId = $('#data-farmer-id').attr('data-farmer-id');
+				$('#farmer').attr('value', dataFarmerId);
+				$('#farmer option[value="' + dataFarmerId + '"]').prop('selected', true);
+			}, 50);
+		}
 	});
 
 }
