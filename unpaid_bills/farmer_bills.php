9e76e714708605911b0abdaba2dec6474a12f23a
Eshwar Chandra <eshwar@useblitz.com>
2024-01-07 19:57:00 +0530

    Unpaid bill table UI

--- unpaid_bills/farmer_bills.php
+++ unpaid_bills/farmer_bills.php
@@ -1,3 +1,37 @@
-<div>
-	Unpaid farmer bills
+<?php
+error_reporting(E_ERROR | E_PARSE);
+require("../../conn.php");
+require_once("../../platform/query.php");
+require_once("../../platform/escape_data.php");
+?>
+<div class="wa bce brd_b">
+  <div class="wa p10 cf tc bbg dontPrint">Unpaid Farmer Bills</div>
+  <div id="result"></div>
+  <div class="pt20 pb20">
+    <form id="auctionListForm" class="dontPrint">
+      <table cellpadding="5" align="center">
+        <tr>
+          <td width="200px">Select Date</td>
+          <td>
+            <select id="weightDate" onchange="ajaxPost('unpaid_bills/farmer_bills_q.php','weightDate','getResult');">
+              <option value="">Select Date</option>
+              <?php
+              $datesArray;
+              $db = new query($con);
+              $records = $db->unique_rows('date', 'lots');
+              for ($i = 0; $i < count($records); $i++) {
+                $date = $records[$i]['date'];
+                ?>
+                <option value="<?php echo $date; ?>"><?php echo $date; ?></option>
+                <?php
+              }
+              ?>
+            </select>
+          </td>
+        </tr>
+      </table>
+    </form>
+    
+    <div id="getResult"></div>
+  </div>
 </div>
---
+++ unpaid_bills/farmer_bills_q.php
@@ -0,0 +1,35 @@
+<?php
+error_reporting(E_ERROR | E_PARSE);
+$date = $_REQUEST['weightDate'];
+require("../../conn.php");
+require_once("../../platform/query.php");
+require_once("../../platform/escape_data.php");
+require_once("../../functions/functions.php");
+$db = new query($con);
+
+$records = $db->select("*","farmer_bills","date='".$date."'");
+if (!empty($date))
+{
+  var_dump($records);
+  for ($i=0;$i<count($records);$i++)
+  {
+    $farmer = new query($con);
+    $farmerRecord = $farmer->select("fid,name,village_id","farmers","id=".$records[$i]['farmer_id']);
+    $farmerName = ucwords($farmerRecord[0]['name'])." (".$farmerRecord[0]['fid'].")";
+    $villageId = $farmerRecord[0]['village_id'];
+
+    $village = new query($con);
+    $villageRecord = $farmer->select("village","villages","id=".$villageId);
+    $villageName = ucwords($villageRecord[0]['village']);
+    ?>
+    <div class="unpaid-bills flex">
+      <div>
+        <?php echo $farmerName; ?>
+      </div>
+      <div>
+        <?php echo $villageName; ?>
+      </div>
+    </div>
+    <?php
+  }
+}
