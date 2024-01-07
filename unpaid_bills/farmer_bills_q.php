a8ab6d69b0d258bbf075e85d6f0d397f998df864
Eshwar Chandra <eshwar@useblitz.com>
2024-01-07 20:27:24 +0530

    safe checks

--- unpaid_bills/farmer_bills_q.php
+++ unpaid_bills/farmer_bills_q.php
@@ -17,19 +17,22 @@ if (!empty($date))
     $farmerRecord = $db->select("fid,name,village_id","farmers","id=".$records[$i]['farmer_id']);
     $farmerName = ucwords($farmerRecord[0]['name'])." (".$farmerRecord[0]['fid'].")";
 
-    var_dump($farmerRecord);
-    $villageId = $farmerRecord[0]['village_id'];
-    $villageRecord = $db->select("village","villages","id=".$villageId);
-    $villageName = ucwords($villageRecord[0]['village']);
-    ?>
-    <div class="unpaid-bills flex">
-      <div>
-        <?php echo $farmerName; ?>
-      </div>
-      <div>
-        <?php echo $villageName; ?>
-      </div>
-    </div>
-    <?php
+    if (!is_null($farmerRecord)) {
+      $villageId = $farmerRecord[0]['village_id'];
+      $villageRecord = $db->select("village","villages","id=".$villageId);
+      $villageName = ucwords($villageRecord[0]['village']);
+      if (!is_null($villageRecord)) {
+        ?>
+        <div class="unpaid-bills flex">
+          <div>
+            <?php echo $farmerName; ?>
+          </div>
+          <div>
+            <?php echo $villageName; ?>
+          </div>
+        </div>
+        <?php
+      }
+    }
   }
 }
